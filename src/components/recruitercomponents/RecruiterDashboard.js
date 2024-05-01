2b4dc5c6be2c432bc0352a9490da8ef6f4f2c5dc
Yendluri Anudeep <anudeepandy33@gmail.com>
2024-05-01 19:13:04 +0530

    completed

--- src/components/recruitercomponents/RecruiterDashboard.js
+++ src/components/recruitercomponents/RecruiterDashboard.js
@@ -11,6 +11,7 @@ function RecruiterDashboard() {
     const [contActiveJobs, setActiveCountJobs] = useState(0);
     const [contJobApplicants, setJobApplicants] = useState(0);
     const [countInterviews, setInterviews] = useState(0);
+    const [countSavedJobs, setSavedJobsCount] = useState(0);
     const [applicants, setApplicants] = useState([]);
     const navigate = useNavigate();
 
@@ -42,6 +43,16 @@ function RecruiterDashboard() {
                 console.error('Error fetching team members:', error);
             });
     }, [user.id]);
+
+    useEffect(() => {
+      axios.get(`${apiUrl}/savedjob/count`)
+          .then((response) => {
+              setSavedJobsCount(response.data);
+          })
+          .catch((error) => {
+              console.error('Error fetching saved jobs count:', error);
+          });
+  }, []); 
 
     // useEffect(() => {
     //     const jwtToken = localStorage.getItem('jwtToken');
@@ -220,6 +231,31 @@ function RecruiterDashboard() {
                 <h4 className="title-count">Interviews</h4>
               </div>
             </div>
+
+            <div className="box-icon wrap-counter flex">
+              <div className="icon style4">
+                <span className="icon-bag">
+                  <svg
+                    width={36}
+                    height={48}
+                    viewBox="0 0 36 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M34.3496 0H2.34998C1.92564 0 1.51868 0.168569 1.21862 0.468623C0.918569 0.768678 0.75 1.17564 0.75 1.59998V46.3994C0.749887 46.7009 0.834958 46.9963 0.99541 47.2515C1.15586 47.5068 1.38517 47.7115 1.6569 47.8421C1.92863 47.9727 2.23173 48.0239 2.53128 47.9897C2.83082 47.9555 3.11462 47.8374 3.34997 47.649L18.3498 35.6476L33.3496 47.6474C33.5848 47.8357 33.8685 47.9538 34.1679 47.9881C34.4673 48.0223 34.7703 47.9712 35.0419 47.8408C35.3136 47.7104 35.5429 47.506 35.7035 47.2509C35.8641 46.9959 35.9494 46.7008 35.9496 46.3994V1.59998C35.9496 1.17564 35.781 0.768678 35.4809 0.468623C35.1809 0.168569 34.7739 0 34.3496 0Z"
+                      fill="#FFB321"
+                    />
+                  </svg>
+                </span>
+              </div>
+              <div className="content">
+              <h3> {countSavedJobs}</h3>
+                <h4 className="title-count">Saved Jobs</h4>
+              </div>
+            </div>
+
+
           </div>
         </div>
       </div>
